87dfbe328a7024235277a5b3b2aa0e2d6409c600
Magdy156 <magdyhamdy156@gmail.com>
2024-08-12 03:23:55 +0300

    task0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+function sendRequest (url) {
+  const req = require('request');
+  return new Promise((resolve, reject) => {
+    req.get(url, (error, res, body) => {
+      if (error) reject(error);
+      else resolve(JSON.parse(body));
+    });
+  });
+}
+
+async function run () {
+  if (process.argv < 3) return;
+
+  const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+  const movie = await sendRequest(movieUrl);
+
+  if (movie.characters === undefined) return;
+  for (const characterUrl of movie.characters) {
+    const character = await sendRequest(characterUrl);
+    console.log(character.name);
+  }
+}
+run();
